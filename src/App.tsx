99f0f802f0df8d4eb20cdb43bbe3fbab672f3d75
Gabriel Canali <gabrielcanali.dev@gmail.com>
2023-09-12 07:31:23 -0300

    feat(in progress): add initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-export function App() {
-  const [count, setCount] = useState(0)
+import { Github } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
 
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido com 💙 no NLW da Rocketseat</span>
+
+          <Separator orientation="vertical" className="h-6"/>
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2"></Github>
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1">
+
+        </div>
+        <aside className="w-80">
+
+        </aside>
+      </main>
+    </div>
   )
 }
